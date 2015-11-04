bb7b5564e81330602f7f6f3eb0cebe82dc6014a4
Skivis <siim.kivistik@gmail.com>
2015-11-05 00:44:59 +0100

    Filled out process.py

--- client/app/process.py
+++ client/app/process.py
@@ -1,11 +1,22 @@
-import sys
+import psutil
 
-try:
-    import psutil
-except ImportError as e:
-    print e, "(pip install psutil)"
-    sys.exit()
+from psutil import AccessDenied, NoSuchProcess
 
 
 class Process(psutil.Process):
-    pass
+
+    def __init__(self, name):
+        super(Process, self).__init__(self._pid(name))
+
+    def _pid(self, name):
+        for process in psutil.process_iter():
+            try:
+                if name in process.name():
+                    return process.pid
+            except AccessDenied as e:
+                print "access denied", e
+
+        raise NoSuchProcess(None, name=name, msg="process not found")
+
+    def update(self):
+        print self.cpu_percent(interval=None)
