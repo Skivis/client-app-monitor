d4e284a766fbe2f6af05c1dcddd5ff9c0f542d24
Skivis <siim.kivistik@gmail.com>
2015-11-05 16:35:25 +0100

    Added db table for logs

--- server/dbsetup.py
+++ server/dbsetup.py
@@ -5,9 +5,20 @@ con = sqlite3.connect('data.db')
 
 con.execute("""
 CREATE TABLE clients (
-    id INTEGER PRIMARY KEY,
-    client_id INTEGER NOT NULL,
-    platform char(100) NOT NULL
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  client_id VARCHAR NOT NULL UNIQUE,
+  platform VARCHAR NOT NULL
+)""")
+
+con.execute("""
+CREATE TABLE logs (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  client_id VARCHAR NOT NULL,
+  level VARCHAR NOT NULL,
+  cpu_percent REAL NOT NULL,
+  memory_percent REAL NULL,
+  num_threads INTEGER,
+  time VARCHAR NOT NULL
 )""")
 
 con.commit()
